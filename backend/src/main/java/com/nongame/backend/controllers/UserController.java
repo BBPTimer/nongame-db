febe40cd6ce803cf01297939064fc25cc0cb6a2f
BBPTimer <greg.weseloh@gmail.com>
2025-09-28 08:09:54 -0500

    Updated UserController methods to return ResponseEntity; added POST, PUT, and DELETE methods

--- backend/src/main/java/com/nongame/backend/controllers/UserController.java
+++ backend/src/main/java/com/nongame/backend/controllers/UserController.java
@@ -2,8 +2,14 @@ package com.nongame.backend.controllers;
 
 import com.nongame.backend.models.User;
 import com.nongame.backend.repositories.UserRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
@@ -11,15 +17,49 @@ public class UserController {
     @Autowired
     UserRepository userRepository;
 
-    @GetMapping("/details/{userId}")
-    public User getUserByID(@PathVariable int userId) {
-        return userRepository.findById(userId).orElse(null);
+    @GetMapping("")
+    public ResponseEntity<?> getAllUsers() {
+        List<User> allUsers = userRepository.findAll();
+        return new ResponseEntity<>(allUsers, HttpStatus.OK);
     }
 
-    @PostMapping("/add")
-    public String addNewUser(@RequestParam String username, String password) {
-        User newUser = new User(username, password);
-        userRepository.save(newUser);
-        return "User added: " + newUser;
+    @GetMapping(value = "/details/{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getUserByID(@PathVariable int userId) {
+        User user = userRepository.findById(userId).orElse(null);
+        if (user == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        }
+    }
+
+    @PostMapping(value = "/add", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> addNewUser(@Valid @RequestBody User user) {
+        userRepository.save(user);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/updatePW/{userId}", consumes=MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> updatePW(@PathVariable int userId, @Valid @RequestBody User user) {
+        User updatedUser = userRepository.findById(userId).orElse(null);
+        if (updatedUser == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        else {
+            updatedUser.setPassword(user.getPassword());
+            userRepository.save(updatedUser);
+            return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+        }
+    }
+
+    @DeleteMapping(value = "/delete/{userId}")
+    public ResponseEntity<?> deleteUser(@PathVariable int userId) {
+        User user = userRepository.findById(userId).orElse(null);
+        if (user == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            userRepository.deleteById(userId);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
     }
 }
