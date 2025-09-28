74b209972042e3921c56e02cf81c12313fa943df
BBPTimer <greg.weseloh@gmail.com>
2025-09-28 08:34:02 -0500

    Updated DeckController and PromptController methods to return ResponseEntity; added POST, PUT, and DELETE methods

--- backend/src/main/java/com/nongame/backend/controllers/DeckController.java
+++ backend/src/main/java/com/nongame/backend/controllers/DeckController.java
@@ -1,13 +1,65 @@
 package com.nongame.backend.controllers;
 
+import com.nongame.backend.models.Deck;
 import com.nongame.backend.repositories.DeckRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/decks")
 public class DeckController {
     @Autowired
     DeckRepository deckRepository;
+
+    @GetMapping("")
+    public ResponseEntity<?> getAllDecks() {
+        List<Deck> allDecks = deckRepository.findAll();
+        return new ResponseEntity<>(allDecks, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/details/{deckId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getDeckByID(@PathVariable int deckId) {
+        Deck deck = deckRepository.findById(deckId).orElse(null);
+        if (deck == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<>(deck, HttpStatus.OK);
+        }
+    }
+
+    @PostMapping(value = "/add", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> addNewDeck(@Valid @RequestBody Deck deck) {
+        deckRepository.save(deck);
+        return new ResponseEntity<>(deck, HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/update/{deckId}", consumes=MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> updatePW(@PathVariable int deckId, @Valid @RequestBody Deck deck) {
+        Deck updatedDeck = deckRepository.findById(deckId).orElse(null);
+        if (updatedDeck == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        else {
+            updatedDeck.setDeckName(deck.getDeckName());
+            deckRepository.save(updatedDeck);
+            return new ResponseEntity<>(updatedDeck, HttpStatus.OK);
+        }
+    }
+
+    @DeleteMapping(value = "/delete/{deckId}")
+    public ResponseEntity<?> deleteDeck(@PathVariable int deckId) {
+        Deck deck = deckRepository.findById(deckId).orElse(null);
+        if (deck == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            deckRepository.deleteById(deckId);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+    }
 }
--- backend/src/main/java/com/nongame/backend/controllers/PromptController.java
+++ backend/src/main/java/com/nongame/backend/controllers/PromptController.java
@@ -1,13 +1,65 @@
 package com.nongame.backend.controllers;
 
+import com.nongame.backend.models.Prompt;
 import com.nongame.backend.repositories.PromptRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/prompts")
 public class PromptController {
     @Autowired
     PromptRepository promptRepository;
+
+    @GetMapping("")
+    public ResponseEntity<?> getAllPrompts() {
+        List<Prompt> allPrompts = promptRepository.findAll();
+        return new ResponseEntity<>(allPrompts, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/details/{promptId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getPromptByID(@PathVariable int promptId) {
+        Prompt prompt = promptRepository.findById(promptId).orElse(null);
+        if (prompt == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<>(prompt, HttpStatus.OK);
+        }
+    }
+
+    @PostMapping(value = "/add", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> addNewPrompt(@Valid @RequestBody Prompt prompt) {
+        promptRepository.save(prompt);
+        return new ResponseEntity<>(prompt, HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/update/{promptId}", consumes=MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> updatePW(@PathVariable int promptId, @Valid @RequestBody Prompt prompt) {
+        Prompt updatedPrompt = promptRepository.findById(promptId).orElse(null);
+        if (updatedPrompt == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        else {
+            updatedPrompt.setPromptText(prompt.getPromptText());
+            promptRepository.save(updatedPrompt);
+            return new ResponseEntity<>(updatedPrompt, HttpStatus.OK);
+        }
+    }
+
+    @DeleteMapping(value = "/delete/{promptId}")
+    public ResponseEntity<?> deletePrompt(@PathVariable int promptId) {
+        Prompt prompt = promptRepository.findById(promptId).orElse(null);
+        if (prompt == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            promptRepository.deleteById(promptId);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+    }
 }
