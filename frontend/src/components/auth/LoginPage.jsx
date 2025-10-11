3f84e8f0a1607157cc6490b0f720a64f94a38d54
BBPTimer <greg.weseloh@gmail.com>
2025-10-11 10:22:04 -0500

    Added LoginPage component

---
+++ frontend/src/components/auth/LoginPage.jsx
@@ -0,0 +1,71 @@
+import { Link, useNavigate } from "react-router";
+import { useContext, useState } from "react";
+import PasswordInput from "./PasswordInput";
+import { requestLogin } from "../../services/authService";
+import {
+  getEmailFromStorage,
+  setEmailInStorage,
+  setTokenInStorage,
+} from "../../services/storageService";
+import { AuthContext } from "../../contexts/AuthContext";
+import { readFormData } from "../../common/utils";
+
+const LoginPage = () => {
+  const { setAuth } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  const [submitting, setSubmitting] = useState(false);
+
+  const logInUser = async (email, password) => {
+    try {
+      let response = await requestLogin({ email, password });
+      if (response?.status === 200) {
+        let token = response.data.token;
+        setEmailInStorage(email);
+        setTokenInStorage(token);
+        setAuth({ token, email, isAuthenticated: true });
+        navigate("/custom");
+      }
+    } catch (error) {
+      console.error(error.message);
+      alert("Invalid credentials");
+      setSubmitting(false);
+    }
+  };
+
+  const handleLogin = (event) => {
+    // Prevent form submission
+    event.preventDefault();
+    // Prevent multiple submissions
+    setSubmitting(true);
+    // Read form data
+    const formJson = readFormData(event);
+    logInUser(formJson.email, formJson.password);
+  };
+
+  return (
+    <main>
+      <h1>Log In</h1>
+      <form onSubmit={handleLogin} className="white-bg">
+        <label htmlFor="email">Email</label>
+        <input id="email" name="email" type="email" defaultValue={getEmailFromStorage()} required />
+        <br />
+
+        <PasswordInput id="password" label="Password" />
+        <br />
+
+        <button type="submit" disabled={submitting}>
+          Log In
+        </button>
+      </form>
+      <br />
+      
+      <span>
+        Don't have an account? <Link to="/register">Register here.</Link>
+      </span>
+    </main>
+  );
+};
+
+export default LoginPage;
